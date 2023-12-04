0ddb9d97123d863f521a987cff90ec94956afb59
charlesilieff <95299358+charlesilieff@users.noreply.github.com>
2023-12-04 14:34:50 +0100

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -18,14 +18,13 @@ const App: React.FC = () => {
   };
 
   // BACKEND :
-  const serverUrl = 'http://83.113.50.18:3000/'
   const serverUrlSocket = 'http://83.113.50.18:3001/'
 
   // REF :
   const remoteVideoRef = useRef<HTMLVideoElement>(null);
 
-  const [roomId, setRoomId] = useState('')
-  const [peerConnection, setPeerConnection] = useState<RTCPeerConnection | null>(null)
+
+
 
 
   const remoteStream = new MediaStream()
@@ -54,7 +53,7 @@ const App: React.FC = () => {
   async function handleStartBroadcast(peerConnection: RTCPeerConnection, socket: Socket) {
     const offer = await peerConnection.createOffer();
     await peerConnection.setLocalDescription(offer);
-    setPeerConnection(peerConnection)
+
     console.log("READY TO SEND OFFER : ", offer);
 
     // Send the offer to the remote peer
@@ -62,24 +61,7 @@ const App: React.FC = () => {
     // Example: socket.emit('offer', offer);
     socket.emit('save room with offer', { offer })
 
-    // try {
-    //   const response = await fetch(serverUrl + "save-room-with-offer", {
-    //     method: 'POST',
-    //     headers: {
-    //       'Content-Type': 'application/json',
-    //     },
-    //     body: JSON.stringify({ offer }),
-    //   });
-    //   if (!response.ok) {
-    //     throw new Error('Request failed');
-    //   }
-    //   const data: any = await response.json();
-    //   setRoomId(data.data)
-    //   console.log("Fetch save room with offer response : ", data);
-    // } catch (error) {
-    //   console.error('An error occurred:', error);
-    //   throw error;
-    // }
+
 
   }
 
@@ -116,23 +98,7 @@ const App: React.FC = () => {
         // Example: socket.emit('candidate', event.candidate.toJSON());
         socket.emit('save caller candidate', event.candidate)
 
-        // try {
-        //   const response = await fetch(serverUrl + "save-caller-candidates", {
-        //     method: 'POST',
-        //     headers: {
-        //       'Content-Type': 'application/json',
-        //     },
-        //     body: JSON.stringify(event.candidate),
-        //   });
-        //   if (!response.ok) {
-        //     throw new Error('Request failed');
-        //   }
-        //   const data: RTCIceCandidate = await response.json();
-        //   console.log("Fetch save caller candidates response : ", data);
-        // } catch (error) {
-        //   console.error('An error occurred:', error);
-        //   throw error;
-        // }
+
 
       } else {
         console.log('ICE candidate gathering completed.');
@@ -148,59 +114,7 @@ const App: React.FC = () => {
 
   };
 
-  // const getRemoteStream = async (roomId: string) => {
 
-  //   try {
-  //     const response = await fetch(serverUrl + "save-room-with-answer/" + roomId, {
-  //       method: 'GET',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //         'Access-Control-Allow-Origin': '*'
-  //       }
-  //     });
-  //     if (!response.ok) {
-  //       throw new Error('Request failed');
-  //     }
-  //     const responseJSON = await response.json();
-  //     console.log("Fetch get room with answer response : ", responseJSON.data.answer);
-
-  //     const rtcSessionDescription = new RTCSessionDescription(responseJSON.data.answer);
-  //     if (peerConnection)
-  //       await peerConnection.setRemoteDescription(rtcSessionDescription);
-
-  //   } catch (error) {
-  //     console.error('An error occurred:', error);
-  //     throw error;
-  //   }
-
-  //   try {
-  //     const response = await fetch(serverUrl + "get-callee-candidates", {
-  //       method: 'GET',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //         'Access-Control-Allow-Origin': ''
-  //       }
-  //     });
-  //     if (!response.ok) {
-  //       throw new Error('Request failed');
-  //     }
-  //     const responseJSON: { data: RTCIceCandidateInit[] } = await response.json();
-  //     console.log("Fetch get callee candidates response : ", responseJSON);
-
-  //     if (peerConnection) {
-  //       responseJSON.data.map(async candidate => {
-  //         console.log("Testing candidate : ", candidate);
-
-  //         await peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
-  //       })
-  //     }
-
-  //   } catch (error) {
-  //     console.error('An error occurred:', error);
-  //     throw error;
-  //   }
-
-  // }
 
   return (
     <div>

00dc5cc66fa4de5f9dacbb30fb8c9263952961b5
charlesilieff <95299358+charlesilieff@users.noreply.github.com>
2023-12-05 11:51:04 +0100

    stade is working

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App: React.FC = () => {
 
   // REF :
   const remoteVideoRef = useRef<HTMLVideoElement>(null);
-  const stadeRemoteVideoRef = useRef<HTMLVideoElement>(null);
+
   const fanPeerConnection = new RTCPeerConnection(configurationIceServer);
   const stadePeerConnection = new RTCPeerConnection(configurationIceServer);
   useEffect(() => {
@@ -39,7 +39,7 @@ const App: React.FC = () => {
 
     const remoteStream = new MediaStream()
 
-    const initializeMediaStream = async (socket: Socket, remoteStream: MediaStream) => {
+    const initializeMediaStream = async (socket: Socket, fanRemoteStream: MediaStream) => {
       try {
 
         const localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
@@ -59,15 +59,23 @@ const App: React.FC = () => {
           })
 
         })
-
+        socket.on('send callee candidate', async (data: any) => {
+          console.log(`Got new FAN remote ICE candidate: ${JSON.stringify(data)}`);
+          await fanPeerConnection.addIceCandidate(new RTCIceCandidate(data.candidate));
+          // remoteStream.getTracks().forEach((track) => {
+          //   console.log("Add remote stream track to peer connexion", track);
+          //   stadePeerConnection.addTrack(track, remoteStream);
+          // });
+        })
 
         fanPeerConnection.addEventListener('track', event => {
           event.streams[0].getTracks().forEach(track => {
-            remoteStream.addTrack(track);
+            fanRemoteStream.addTrack(track);
           });
           if (remoteVideoRef.current) {
             remoteVideoRef.current.srcObject = event.streams[0];
           }
+
         });
 
 
@@ -97,40 +105,39 @@ const App: React.FC = () => {
 
 
 
-
+        let toDelete: RTCRtpSender;
         if (localStream) {
           localStream.getTracks().forEach((track) => {
             console.log("Add local stream track to STADE peer connexion", track);
-            stadePeerConnection.addTrack(track, localStream);
+
+            toDelete = stadePeerConnection.addTrack(track, localStream);
           });
         }
         socket.on('send stade room with answer', async (data: any) => {
 
           const rtcSessionDescription = new RTCSessionDescription(data);
           console.log('Connection to STADE', stadePeerConnection.currentRemoteDescription)
-
+          if (fanRemoteStream) {
+            console.log("REMOTE STREAM ADDED TO STADE CONNEXION", fanRemoteStream)
+            fanRemoteStream.getTracks().forEach((track) => {
+              stadePeerConnection.removeTrack(toDelete)
+              stadePeerConnection.addTrack(track, fanRemoteStream);
+            });
+          }
           await stadePeerConnection.setRemoteDescription(rtcSessionDescription);
-          remoteStream.getTracks().forEach((track) => {
-            console.log("Add remote stream track to STAAAADDDEEE peer connexion", track);
-            stadePeerConnection.addTrack(track, remoteStream);
-          });
+
         })
 
         socket.on('send stade callee candidate', async (data: any) => {
           console.log(`Got new stade remote ICE candidate: ${JSON.stringify(data)}`);
           await stadePeerConnection.addIceCandidate(new RTCIceCandidate(data.candidate));
-          // remoteStream.getTracks().forEach((track) => {
-          //   console.log("Add remote stream track to peer connexion", track);
-          //   stadePeerConnection.addTrack(track, remoteStream);
-          // });
+
         })
         stadePeerConnection.addEventListener('track', event => {
           event.streams[0].getTracks().forEach(track => {
-            remoteStream.addTrack(track);
+            fanRemoteStream.addTrack(track);
           });
-          if (stadeRemoteVideoRef.current) {
-            stadeRemoteVideoRef.current.srcObject = event.streams[0];
-          }
+
         });
 
 
@@ -146,12 +153,7 @@ const App: React.FC = () => {
         });
 
 
-        // if (remoteStream) {
-        //   console.log("REMOTE STREAM ADDED TO STADE CONNEXION", remoteStream)
-        //   remoteStream.getTracks().forEach((track) => {
-        //     stadePeerConnection.addTrack(track, remoteStream);
-        //   });
-        // }
+
 
 
 
@@ -180,8 +182,7 @@ const App: React.FC = () => {
         <h1>REGIE</h1>
         <h2>Remote Video</h2>
         <video ref={remoteVideoRef} autoPlay playsInline muted />
-        <h2>Stade Video</h2>
-        <video ref={stadeRemoteVideoRef} autoPlay playsInline muted />
+
       </div>
     </div>
   );

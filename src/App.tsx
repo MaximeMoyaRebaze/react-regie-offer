069516859bab369294daef9fc3f1b46b9d7f50b8
charlesilieff <95299358+charlesilieff@users.noreply.github.com>
2023-12-08 09:27:55 +0100

    dprint

--- src/App.tsx
+++ src/App.tsx
@@ -21,22 +21,12 @@ export const App: React.FC = () => {
   const [remoteStreams, setRemoteStreams] = React.useState<{ mediaStream: MediaStream; remoteVideoRef: RefObject<HTMLVideoElement>; }[]>([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24].map(() => ({ mediaStream: new MediaStream(), remoteVideoRef: useRef<HTMLVideoElement>(null) })))
 
 
-
-
   // INITIALIZE :
   useEffect(() => {
     const stadePeerConnection = new RTCPeerConnection(Implementations.configurationIceServer);
     setStadePeerConnection(stadePeerConnection);
     const initialize = async () => {
       const socket = io(serverUrlSocket);
-
-
-
-
-
-
-
-
 
       setRemoteStreams(await Promise.all(
 
